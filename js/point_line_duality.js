8e2465ee0be8bc8258106c38418610473d5b9bd5
Julian Heinrich <julian.heinrich@parallelcoordinates.de>
2015-08-05 15:35:01 +1000

    fixed resizing of plots in posts

--- js/point_line_duality.js
+++ js/point_line_duality.js
@@ -7,6 +7,7 @@ var margin = {top: 20, right: 20, bottom: 20, left: 20},
     height = svgHeight - margin.top - margin.bottom;
 
 var domain = [0, 10];
+var points = [];
 
 var click = function() {
    // Ignore the click event if it was suppressed
@@ -16,43 +17,16 @@ var click = function() {
   var x1 = c[0] - margin.left;
   var x2 = c[1] - margin.top;
   
-  var point = sampleSVG.append("circle")
-      // .data([{x1:x1, x2:x2}])
-      .style("stroke", "gray")
-      .style("fill", "gray")  
-      .attr("transform", "translate(" + x1 + "," + x2 + ")")
-      .attr("r", 4)
-      .attr("class", "dot")
-      .style("cursor", "pointer")
-      .call(drag);
-
-  // create a parallel coordinates system by specifying
-  // the axis-spacing vector [left axis, right axis]
-  var d = [margin.left, width];
-  // var pc = paco(d);
-
-  // create a 1-flat (a point) in
-  // N=2 dimensional space from c.
-  // This creates a set of coefficients as
-  // described above (c11, c10, c22, c20)
-  // var point = paco.flat(1, 2, c);
-
-  // get coordinates of points on the axes
-  // (indexed points with one index), then
-  // draw a line segment connecting these points
-  // ip = pc.ip(point);
-  // ip = paco.ip(x1, x2, d);
-
-  var line = lineSVG.append("line")
-    .style("stroke", "gray")
-    .attr("x1", d[0])
-    .attr("y1", yScale(xScale.invert(x1)))
-    .attr("x2", d[1])
-    .attr("y2", x2);  // same as yScale(yScale.invert(x2))
-
-    // add the line as data to the point to make it accessible in dragmove
-    point.data([line]);
+  //drawSample(xScale.invert(x1), yScale.invert(x2));
+  points.push([xScale.invert(x1), xScale.invert(x2)]);
+  update();
 }
+
+// create a parallel coordinates system by specifying
+// the axis-spacing vector [left axis, right axis]
+var d = d3.scale.linear()
+  .domain([0, 1]).
+  range([margin.left, width]);
 
 // setup x
 var xScale = d3.scale.linear()
@@ -80,8 +54,6 @@ var sampleSVG = d3.select("#cartesian")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight)
-  //.attr("viewBox", "0 0 " + svgWidth + " " + svgHeight)
-  //.attr("preserveAspectRatio", "xMidYMid meet")
   .on("click", click)
 .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -90,8 +62,6 @@ var lineSVG = d3.select("#parallel")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight)
-  //.attr("viewBox", "0 0 " + svgWidth + " " + svgHeight)
-  //.attr("preserveAspectRatio", "xMidYMid meet")
 .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -105,14 +75,14 @@ sampleSVG.append("g")
   .call(yAxisLeft);
 
 lineSVG.append("g")
-.attr("class", "y axis left")
-.attr("transform", "translate("+margin.left+",0)")
-.call(yAxisLeft); 
+  .attr("class", "y axis left")
+  .attr("transform", "translate("+margin.left+",0)")
+  .call(yAxisLeft); 
 
 lineSVG.append("g")
-.attr("class", "y axis right")
-.attr("transform", "translate("+width+",0)")
-.call(yAxisRight); 
+  .attr("class", "y axis right")
+  .attr("transform", "translate("+width+",0)")
+  .call(yAxisRight); 
 
 var drag = d3.behavior.drag()
             .on("drag", dragmove);
@@ -120,12 +90,12 @@ var drag = d3.behavior.drag()
 function dragmove(d) {
   var x = d3.event.x;
   var y = d3.event.y;
-  d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
-  d.attr("y1", yScale(xScale.invert(x)))
-      .attr("y2", y);
+  d[0] = xScale.invert(x);
+  d[1] = yScale.invert(height - y);
+  update();
 }
 
-function layout() {
+function update() {
   var aspect = svgWidth/svgHeight;
   var targetWidth = $("#parallel").parent().width() * 0.4;
 
@@ -137,6 +107,7 @@ function layout() {
 
   xScale.range([0, width]);
   yScale.range([height, 0]);
+  d.range([margin.left, width]);
 
   sampleSVG.select('.x.axis')
     .attr("transform", "translate(0," + height +")")
@@ -145,6 +116,8 @@ function layout() {
   sampleSVG.select('.y.axis')
     .call(yAxisLeft);
 
+  lineSVG.select('.y.axis.right')
+    .attr("transform", "translate("+width+",0)");
 
   lineSVG.select('.y.axis.left')
     .call(yAxisLeft);  
@@ -152,21 +125,50 @@ function layout() {
   lineSVG.select('.y.axis.right')
     .call(yAxisRight);
 
-  // var cartesian = $("#cartesian svg");
-  // cartesian.attr("width", w);
-  // cartesian.attr("height", h);
-  // cartesian.on("click", click);
+  var cartesian = d3.select("#cartesian svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-  // var parallel = $("#parallel svg");
-  // parallel.attr("width", w);
-  // parallel.attr("height", h);
+  var parallel = d3.select("#parallel svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+  circles = sampleSVG.selectAll(".dot")
+    .data(points);
+
+  circles.enter()
+      .append("circle")
+      .attr("class", "enter");
+
+  circles
+      .style("stroke", "gray")
+      .style("fill", "gray")  
+      .attr("r", 4)
+      .attr("class", "dot")
+      .style("cursor", "pointer")
+      .attr("cx", function(d) { return xScale(d[0]); })
+      .attr("cy", function(d) { return height - yScale(d[1]); })
+      .call(drag);
+
+  lines = lineSVG.selectAll(".line").data(points);
+  lines.enter()
+    .append("line")
+    .attr("class", "enter");
+
+  lines
+    .style("stroke", "gray")
+    .attr("class", "line")
+    .attr("x1", d(0))
+    .attr("y1", function(d) { return yScale(d[0]); })
+    .attr("x2", d(1))
+    .attr("y2", function(d) { return height - yScale(d[1]); });  // same as yScale(yScale.invert(x2))
 
 }
 
 $(document).ready(function() {
 
-  //$(window).resize(layout);
-  //layout();
+  $(window).resize(update);
+  update();
 
 });
 
--- tutorial/_posts/2015-08-02-mapping-points.md
+++ tutorial/_posts/2015-08-02-mapping-points.md
@@ -6,66 +6,65 @@ layout: post
 
 Visualizing points in more than three dimensions seems to be impossible, because each axis needs to be orthogonal to every other axis in Cartesian coordinates. A straight-forward solution to this problem is to describe one N-dimensional point using N one-dimensional points - one for each dimension:
 
-<div class="container-fluid">
-<div class="row">
-<div id="parallel-points" class="img-responsive parcoords tutorial"></div>
-</div>
-</div>
+<div id="parallel-points" class="parcoords tutorial"></div>
+
 <link rel="stylesheet" type="text/css"
   href="{{site.baseurl}}/css/tutorial.css">
-<!-- <script src="/js/pacolib.js"/> -->
-<!-- <script src="/js/parallel-points.js"/> -->
 <link rel="stylesheet" type="text/css"
   href="{{site.baseurl}}/paco/css/d3.parcoords.css">
 <script src="{{site.baseurl}}/paco/js/d3.parcoords.js"></script>
 
 <script>
 
-var data = [
-  [3, 7, 5, 1, 9]
-];
+var mappingPoints = (function() {
 
-var pc = d3.parcoords({
-	dimensionTitles: {
-		0: "Dimension 1",
-		1: "Dimension 2",
-		2: "Dimension 3",
-		3: "Dimension 4",
-		4: "Dimension 5"
-	}
-})("#parallel-points")
-  .data(data)
-  .detectDimensions()
-  .autoscale();
+	var data = [
+	  [3, 7, 5, 1, 9]
+	];
 
-// we need to assign a domain to every dimension because
-// for single values, d3.parcoords.js uses ordinal scales
-// for a dimension
-var domain = [0,1,2,3,4,5,6,7,8,9,10];
+	var pc = d3.parcoords({
+		dimensionTitles: {
+			0: "Dimension 1",
+			1: "Dimension 2",
+			2: "Dimension 3",
+			3: "Dimension 4",
+			4: "Dimension 5"
+		}
+	})("#parallel-points")
+	  .data(data)
+	  .detectDimensions()
+	  .autoscale();
 
-var layout = function() {
-	var aspect = 5;
-	var w = $("#parallel-points").width();
-	var h = w / aspect;
-	//$("#parallel-points").height(h);
-	
-	pc.width(w);
-	//   .height(h);
+	// we need to assign a domain to every dimension because
+	// for single values, d3.parcoords.js uses ordinal scales
+	// for a dimension
+	var domain = [0,1,2,3,4,5,6,7,8,9,10];
 
-	// BUG in d3.parcoords.js: 
-	// resize resets all scales
-	pc.dimensions().forEach(function(d) {
-		pc.scale(d, domain);
-	});
+	var layout = function() {
+		var aspect = 5;
+		var w = $("#parallel-points").width();
+		var h = w / aspect;
+		//$("#parallel-points").height(h);
+		
+		pc.width(w);
+		//   .height(h);
 
-	pc.alpha(0)
-	  .render()
-	  .createAxes()
-	  .ticks(5)
-	  .axisDots(2);
-};
+		// BUG in d3.parcoords.js: 
+		// resize resets all scales
+		pc.dimensions().forEach(function(d) {
+			pc.scale(d, domain);
+		});
 
-$(window).resize(layout);
-layout();
+		pc.alpha(0)
+		  .render()
+		  .createAxes()
+		  // .ticks(5)
+		  .axisDots(2);
+	};
+
+	$(window).resize(layout);
+	layout();
+
+})();
 
 </script>
